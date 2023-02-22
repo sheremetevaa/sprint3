317ef29b4b8ebf437716935702d96d8b5da8a150
sheremetevaa <asya.sheremeteva94@yandex.ru>
2023-02-22 14:31:56 +0300

    task sprint 3

--- sprint3/ViewController.swift
+++ sprint3/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var changeLable: UILabel!
+    private var count: Int = 0
+    
+    @IBAction func tapButton(_ sender: Any) {
+    count += 1
+        changeLable.text = "\(count)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
